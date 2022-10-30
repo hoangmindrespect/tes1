42b7267d63595f25767d9bff469dc5416c390de3
Nguyễn Hoàng Minh <109229679+hoangmindrespect@users.noreply.github.com>
2022-10-30 21:38:08 +0700

    Add files via upload

---
+++ MasterLibrary/MasterLibrary/MasterLibrary/ConnectDatabase/ConnectToLoginDB.cs
@@ -0,0 +1,65 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterLibrary.ConnectDatabase
+{
+    public class ConnectToLoginDB
+    {
+        private static string connectionstring = "Server=tcp:masterlibdatabase.database.windows.net,1433;Initial Catalog=log_db;Persist Security Info=False;User ID=hoangminh;Password=Masterlib9;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        private static SqlConnection sqlconnection = null;
+
+        public ConnectToLoginDB()
+        {
+            OpenConnect();
+        }
+
+        private static void OpenConnect()
+        {
+            sqlconnection = new SqlConnection(connectionstring);
+
+            sqlconnection.Open();
+            if (sqlconnection.State == ConnectionState.Open)
+                sqlconnection.Close();
+        }
+
+        /// <summary>
+        ///  Get data from database
+        /// </summary>
+        /// <param name="stringsql"></param>
+        /// <returns></returns>
+        public static DataTable DataTransport(string stringsql)
+        {
+            OpenConnect();
+            SqlDataAdapter adapter = new SqlDataAdapter(stringsql, sqlconnection);
+            DataTable dt = new DataTable();
+            dt.Clear();
+            adapter.Fill(dt);
+            return dt;
+        }
+
+        /// <summary>
+        /// insert data to database
+        /// </summary>
+        /// <param name="stringsql"></param>
+        /// <returns></returns>
+        public static int DataExcution(string stringsql)
+        {
+            int result = 0;
+            OpenConnect();
+            if (sqlconnection.State == ConnectionState.Closed)
+                sqlconnection.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = sqlconnection;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = stringsql;
+            result = cmd.ExecuteNonQuery();
+            return result;
+        }
+    }
+}
---
+++ MasterLibrary/MasterLibrary/MasterLibrary/View/Register.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MasterLibrary.View
+{
+    /// <summary>
+    /// Interaction logic for Register.xaml
+    /// </summary>
+    public partial class Register : Window
+    {
+        public Register()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonregister_Click(object sender, RoutedEventArgs e)
+        {
+            if (!AllowRegister())
+                return;
+            string sql = "INSERT INTO UserInf VALUES (" + "'" + txb_name.Text + "'" + "," + "'" + txb_dateofbirth.Text + "'" + "," + 
+                "'" + txb_email.Text + "'" + "," + "'" + txb_username.Text + "'" + "," + "'" + PasswordBox.Password + "'" + ")";
+            if (ConnectDatabase.ConnectToLoginDB.DataExcution(sql) != 0)
+                MessageBox.Show("Register successful!");
+            else
+                MessageBox.Show("Register unsuccessful");
+        }
+        private bool AllowRegister()
+        {
+            if(ConFPasswordBox.Password != PasswordBox.Password)
+            {
+                ConFPasswordBox.BorderBrush = Brushes.IndianRed;
+                
+                return false;
+            }    
+            if (txb_email.Text.Length == 0)
+            {
+                MessageBox.Show("Fill in your email", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+                txb_email.Focus();
+                return false;
+            }
+            if (txb_username.Text.Length == 0)
+            {
+                MessageBox.Show("Fill in your user name", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+                txb_username.Focus();
+                return false;
+            }
+            if (PasswordBox.Password.Trim().Length == 0)
+            {
+                MessageBox.Show("Fill in your password", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+                PasswordBox.Focus();
+                return false;
+            }
+            if(ConFPasswordBox.Password.Trim().Length == 0)
+            {
+                MessageBox.Show("Confirm your password", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+                ConFPasswordBox.Focus();
+                return false;
+            }     
+            return true;
+        }
+    }
+}
